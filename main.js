4b7619ca4208acc2cb2377893087ebea1ce1eb7b
Asha-art <62064699+Asha-art@users.noreply.github.com>
2020-04-02 13:25:50 -0400

    Adding baabaa black sheep files

---
+++ main.js
@@ -0,0 +1,69 @@
+// function greetOnLoad () {
+//     var name = prompt("Hi! What's your name?");
+//     var myelement = document.getElementById("greeting");
+//     myelement.innerHTML= "Hello " + name + ", it's nice to meet you!";
+// }
+// greetOnLoad();
+
+// var x=2;
+// function Thursday () {
+// 	x=30;
+// }
+
+// x;
+var woolOwners = [
+  {
+	  "master": 1
+  },
+  {
+  	"dame": 1
+  },
+  {
+	"little boy": 1	,
+	 "location": "down the lane"
+	}
+
+];
+
+
+	
+var haveYouAnyWool = function() {
+	for (var i = 0; i < woolOwners.length; i++) {
+	var totalBags = totalBags + i;
+	}
+	return (i);   //
+	
+};
+
+var bags = haveYouAnyWool();
+
+function baabaaBlackSheep() {
+	console.log("BaaBaa BlackSheep have you any wool?");
+	if (bags > 0) {
+		console.log("yes sir, yes sir " + bags + " bags full");  // edited
+  }
+}
+
+
+
+
+function oneForMy() {
+	for (var i = 0; i < 2; i++) {
+		people = Object.keys(woolOwners[i]);
+		var person = people.toString();
+		 console.log("one for my " + person);
+	}
+
+	
+}
+
+baabaaBlackSheep();
+
+oneForMy();
+
+ var boy = Object.keys(woolOwners[2]);
+ var littleBoy = boy[0];
+
+ var whereHeLives =  woolOwners[2].location;
+
+console.log("one for the " + littleBoy + " who lives " + whereHeLives);
